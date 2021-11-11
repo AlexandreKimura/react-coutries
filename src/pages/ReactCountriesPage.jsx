a3a46bb0393dcaa815099a30a5c0e382773d1229
Alexandre Kimura <alexandremkimura@hotmail.com>
2021-11-10 22:09:30 -0300

    Load json and initial input

--- src/pages/ReactCountriesPage.jsx
+++ src/pages/ReactCountriesPage.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react"
 import Header from "../components/Header"
 import Main from "../components/Main"
+import TextInput from "../components/TextInput"
+
+import { allCountries } from "../data/countries"
 
 export default function ReactCountriesPage() {
+
+  const [countryFilter, setCountryFilter] = useState('Argentina')
+
+  function handleCountryFilterChange(newCountryFilter) {
+    setCountryFilter(newCountryFilter)
+  }
+
   return (
     <div>
       <Header>React Countries</Header>
-      <Main>Conteúdo</Main>
+      <Main>
+        <TextInput 
+          id="inputCountryFilter"
+          labelDescription="Informe o nome do país(pelo menos 3 caracteres):" 
+          inputValue={countryFilter}
+          onInputChange={handleCountryFilterChange}
+          autoFocus
+        />
+      </Main>
     </div>
   )
 }
